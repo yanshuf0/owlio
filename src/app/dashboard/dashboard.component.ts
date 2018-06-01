4b060834412430ce13e0f279af9904009ee43d12
Zachary Schwartz <zachscs@gmail.com>
2018-05-31 22:22:11 -0700

    responsive layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,21 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
   throttle = 300;
   scrollDistance = 0;
-  cards = [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 },
-  ];
+  cards = [];
 
-  onScrollDown() {
-    this.cards = this.cards.concat([
-      { title: 'Card 1', cols: 2, rows: 1 },
-      { title: 'Card 2', cols: 1, rows: 1 },
-      { title: 'Card 3', cols: 1, rows: 2 },
-      { title: 'Card 4', cols: 1, rows: 1 },
-    ]);
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.onScrollDown();
+  }
+
+  async onScrollDown() {
+    this.isHandset$.pipe(take(1)).subscribe(isHandset => {
+      if (isHandset) {
+        this.cards = this.cards.concat([
+          { title: 'Card 1', cols: 2, rows: 1 },
+          { title: 'Card 2', cols: 2, rows: 1 },
+          { title: 'Card 3', cols: 2, rows: 1 },
+          { title: 'Card 4', cols: 2, rows: 1 },
+        ]);
+      } else {
+        this.cards = this.cards.concat([
+          { title: 'Card 1', cols: 2, rows: 1 },
+          { title: 'Card 2', cols: 1, rows: 1 },
+          { title: 'Card 3', cols: 1, rows: 2 },
+          { title: 'Card 4', cols: 1, rows: 1 },
+        ]);
+      }
+    })
+
   }
 }
