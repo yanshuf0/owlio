e48a279a441f322c05e6821326195d8fb03288ca
Zachary Schwartz <zachscs@outlook.com>
2018-06-01 09:39:28 -0700

    adjust path and debug

--- server/api/auth/auth.controller.ts
+++ server/api/auth/auth.controller.ts
@@ -2,17 +2,17 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './auth.dto';
 
-@Controller()
+@Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('api/login')
+  @Post('/login')
   async login(@Body() authDto: AuthDto) {
     const authRes = await this.authService.login(authDto);
     return authRes;
   }
 
-  @Post('api/signup')
+  @Post('/signup')
   async signup(@Body() authDto: AuthDto) {
     const authRes = await this.authService.signup(authDto);
     return authRes;
