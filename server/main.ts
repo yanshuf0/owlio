41f9dab88825ed62efe5c7db6c5d8634f6891d4c
Zachary Schwartz <zachscs@outlook.com>
2018-06-01 17:17:18 -0700

    clusters server

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { enableProdMode } from '@angular/core';
+import * as cluster from 'express-cluster';
 
 enableProdMode();
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await cluster((w) => app.listen(3000), {});
 }
 bootstrap();
